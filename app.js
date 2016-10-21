65967c3ffb8981ec8e0998be98cf272d9e45ac66
Jean-Jérôme Pantalacci <jean-jerome.pantalacci-ext@fnac.com>
2016-10-21 14:38:37 +0200

    ajout katas

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
 var input = "5 2 3\n1 3 3";
 var triangle = require('./triangleperimeter');
 var triplets = require('./triplets');
-console.log(triplets.processData(input));
+//var circu = require('./circulararrayrotation');
+//console.log(circu.processData(input));
+
+function filter_list(l) {
+    return l.filter(function (a) { return (typeof a !== 'string'); });
+}
+
+function squareOrSquareRoot(array) {
+    for (var i = 1; i < array.length; i++) {
+        if (Math.sqrt(array[i]) % 1 == 0)
+            array[i] = Math.sqrt(array[i]);
+        else
+            array[i] *= array[i];
+    }
+    return array;
+}
+
+function alphabetPosition(text) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var final = "";
+  for(var i=0; i<text.length; i++)
+  {
+    final += alphabet.indexOf(text.toLowerCase())+1 + " ";
+  }
+  return final.trim();
+}
+
+function tribonacci(signature, n){
+if (n===0) return [];
+if (!signature) signature = [];
+
+  if (n===1) return signature.length == 0 ? [signature[0]] : signature; 
+  if (n===2 && signature.length == 1) return [signature[0],signature[1]];
+  if (n===3 && signature.length == 2) return [signature[0],signature[1], signature[2]];
+  if (signature.length==0) signature.push(signature[0],signature[1],signature[2], signature[0] + signature[1] + signature[2]);
+else if (n > 3) signature.push(signature[signature.length-2] + signature[signature.length-1] + signature[signature.length]);
+  return tribonacci(signature, n-1);
+}
+
+console.log(tribonacci([1,1,1],10));
+
+//console.log(squareOrSquareRoot([4, 3, 9, 7, 2, 1]));
---
+++ circulararrayrotation.js
@@ -0,0 +1,5 @@
+module.exports = {
+	processData: function (input) {
+
+	}
+}
